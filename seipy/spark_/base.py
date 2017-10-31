be16ba2004d562a1a107ec1fd6c0e0b032fb75e1
Seiji Armstrong <seiji@stealthsec.com>
2017-10-31 15:03:10 -0700

    registering sql table temporary view

--- seipy/spark_/base.py
+++ seipy/spark_/base.py
@@ -1,4 +1,5 @@
 
+
 def s3spark_init():
     """
     initialise SparkSession for use with Jupyter and s3 SQL queries
@@ -15,12 +16,29 @@ def s3spark_init():
         .getOrCreate()
 
     hadoopConf = spark.sparkContext._jsc.hadoopConfiguration()
-    print("type in access key yo:")
+    print("type in aws access key yo:")
     myAccessKey = input()
-    print("type in secret key yo:")
+    print("type in aws secret key yo:")
     mySecretKey = input()
 
     hadoopConf.set("fs.s3.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
     hadoopConf.set("fs.s3.awsAccessKeyId", myAccessKey)
     hadoopConf.set("fs.s3.awsSecretAccessKey", mySecretKey)
     return spark
+
+
+def register_sql(spark, files, table_name="table"):
+    """
+    Register a list of files as a SQL temporary view.
+
+    parameters:
+    - files is overloaded: can be one file path or list of file paths.
+    - spark: pyspark.sql.SparkSession
+    - table_name: this is how we will refer to table in SQL query
+    Schema of files must be the same for the table
+
+    Example usage after registering table:
+    >> DF = spark.sql("SELECT * FROM table")
+    """
+    tempFiles = spark.read.csv(files, header=True)
+    tempFiles.createOrReplaceTempView(table_name)
