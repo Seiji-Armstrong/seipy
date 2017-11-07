fd0eead8a50fe3ef9c75ea22bd57f7d3839d72df
Seiji Armstrong <seiji@stealthsec.com>
2017-11-06 19:17:04 -0800

    adding pcap reading capapbilities

---
+++ seipy/pcap/base.py
@@ -0,0 +1,8 @@
+import scapy
+# import scapy_ssl_tls as scl # python 2
+
+def read_pcap(pcap_file):
+    """
+    light wrapper over scapy's pcap reader.
+    """
+    return scapy.utils.rdpcap(pcap_file)
